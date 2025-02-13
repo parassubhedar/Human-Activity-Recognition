0ada3d70b250cec575c27ac5da0350535b4ba224
parassubhedar <parassubhedar@gmail.com>
2025-02-14 03:01:12 +0530

    Added app.py for FastAPI deployment

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Human Activity Recognition API is running!"}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT is not set
+    uvicorn.run(app, host="0.0.0.0", port=port)
