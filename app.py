479cb03d797c6f4b6707df3b4b99e2d5024e18c0
parassubhedar <parassubhedar@gmail.com>
2025-02-14 04:18:03 +0530

    Fixed Procfile and port issue

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def home():
     return {"message": "Human Activity Recognition API is running!"}
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT is not set
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 if PORT is not set
     uvicorn.run(app, host="0.0.0.0", port=port)
